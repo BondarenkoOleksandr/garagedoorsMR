889d66ccbce7f364832765dc68bf9c48964ef661
BondarenkoAlexander <sanyasashkabond32@gmail.com>
2021-09-21 13:53:54 +0300

    Changed ip get

--- src/core/utils.py
+++ src/core/utils.py
@@ -13,7 +13,7 @@ class ExtendedEncoder(DjangoJSONEncoder):
 def get_user_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
+        ip = x_forwarded_for
     else:
         ip = request.META.get('REMOTE_ADDR')
 
