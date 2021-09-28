0568ee1bcf2611162ce19ac1b0f635070d9299bc
BondarenkoAlexander <sanyasashkabond32@gmail.com>
2021-09-28 17:45:08 +0300

    employees list pagination

--- src/employees/api/views.py
+++ src/employees/api/views.py
@@ -1,17 +1,18 @@
-import json
-
-from django.core.serializers.json import DjangoJSONEncoder
 from django.forms import model_to_dict
 from django.http import JsonResponse
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, get_object_or_404
 
+from core.utils import queryset_pagination
 from employees.api.serializers import EmployeeSerializer, ReviewSerializer
 from employees.models import Employee, Review
 
 
 class EmployeeListView(ListAPIView):
-    queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+
+    def get_queryset(self):
+        employees = Employee.objects.all()
+        return queryset_pagination(self.request, employees)
 
 
 class EmployeeReviewCreateAPIView(CreateAPIView):
@@ -20,7 +21,7 @@ class EmployeeReviewCreateAPIView(CreateAPIView):
 
     def post(self, request):
         employee_id = request.POST.get('employee', None)
-        employee = Employee.objects.get(id=employee_id)
+        employee = get_object_or_404(Employee, id=employee_id)
         name = request.POST.get('name', None)
         text = request.POST.get('text', None)
         rating = request.POST.get('rating', 5)
