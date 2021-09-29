c6cc484ae4f182ad8b0c2014250aa126ec9edf91
BondarenkoAlexandr <sanyasashkabond32@gmail.com>
2021-09-29 12:12:09 +0300

    client id changed

--- src/accounts/api/views.py
+++ src/accounts/api/views.py
@@ -16,13 +16,13 @@ class GoogleLoginApi(APIView):
         code = request.GET.get('code')
         error = request.GET.get('error')
 
-        login_url = f'http://localhost:8008/login'
+        login_url = f'http://localhost:8000/login'
 
         if error or not code:
             params = urlencode({'error': error})
             return redirect(f'{login_url}?{params}')
 
-        domain = 'https://garagedoors.fun'
+        domain = 'http://localhost:8000'
         api_uri = reverse('accounts_api:google-login')
         redirect_uri = f'{domain}{api_uri}'
 
@@ -41,7 +41,7 @@ class GoogleLoginApi(APIView):
         # We don't have a sign-up flow.
         user, _ = User.objects.get_or_create(**profile_data)
 
-        response = redirect('https://garagedoors.fun/api/articles/')
+        response = redirect('http://localhost:8000/')
         response = jwt_login(response=response, user=user)
 
         return response
--- src/static/js/main.js
+++ src/static/js/main.js
@@ -9,8 +9,8 @@ function sendReq(){
 
   const params = {
     response_type: 'code',
-    client_id: '551586915430-tqjp7rsp2thrsp71sr9s3ukkb8nkf8eu.apps.googleusercontent.com',
-    redirect_uri: `http://localhost:8008/${redirectUri}`,
+    client_id: '486436967116-tiadudc1f9rmoj9c94rgp86q5ppks5pb.apps.googleusercontent.com',
+    redirect_uri: `http://localhost:8000/${redirectUri}`,
     prompt: 'select_account',
     access_type: 'offline',
     scope
