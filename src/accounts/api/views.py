4b28aa8b67fe26de97b98206b62f070868680ded
BondarenkoAlexandr <sanyasashkabond32@gmail.com>
2021-09-29 12:47:46 +0300

    edit links

--- src/accounts/api/views.py
+++ src/accounts/api/views.py
@@ -16,13 +16,13 @@ class GoogleLoginApi(APIView):
         code = request.GET.get('code')
         error = request.GET.get('error')
 
-        login_url = f'http://localhost:8000/login'
+        login_url = f'https://garagedoors.fun/login'
 
         if error or not code:
             params = urlencode({'error': error})
             return redirect(f'{login_url}?{params}')
 
-        domain = 'http://localhost:8000'
+        domain = 'https://garagedoors.fun'
         api_uri = reverse('accounts_api:google-login')
         redirect_uri = f'{domain}{api_uri}'
 
@@ -41,7 +41,7 @@ class GoogleLoginApi(APIView):
         # We don't have a sign-up flow.
         user, _ = User.objects.get_or_create(**profile_data)
 
-        response = redirect('http://localhost:8000/')
+        response = redirect('https://garagedoors.fun/')
         response = jwt_login(response=response, user=user)
 
         return response
