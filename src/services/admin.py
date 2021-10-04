e4eacd24c398bff42e389053372f8201168576b0
BondarenkoAlexander <sanyasashkabond32@gmail.com>
2021-10-04 18:33:25 +0300

    search in services

--- src/services/admin.py
+++ src/services/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from services.models import Service, ServiceCategory
 
-admin.site.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    search_fields = ['category__name']
+
+
+admin.site.register(Service, ServiceAdmin)
 admin.site.register(ServiceCategory)
