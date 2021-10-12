c3da78f408d48e149c1f370e5b1d58900ff87832
BondarenkoAlexandr <sanyasashkabond32@gmail.com>
2021-10-12 15:33:12 +0300

    change text function

--- src/core/text_changer.py
+++ src/core/text_changer.py
@@ -3,9 +3,9 @@ from django.db.models import CharField, TextField
 
 def change_text(queryset):
     for model in queryset:
-        model.firstscreen.title = model.firstscreen.title.repace('вЂ™', '\'')
-        model.secondscreen.main_title = model.secondscreen.main_title.repace('вЂ™', '\'')
-        model.firstscreen.description = model.firstscreen.description.repace('вЂ™', '\'')
-        model.secondscreen.main_description = model.secondscreen.main_description.repace('вЂ™', '\'')
-        model.description = model.description.repace('вЂ™', '\'')
+        model.firstscreen.title = model.firstscreen.title.replace('вЂ™', '\'')
+        model.secondscreen.main_title = model.secondscreen.main_title.replace('вЂ™', '\'')
+        model.firstscreen.description = model.firstscreen.description.replace('вЂ™', '\'')
+        model.secondscreen.main_description = model.secondscreen.main_description.replace('вЂ™', '\'')
+        model.description = model.description.replace('вЂ™', '\'')
         model.save()
