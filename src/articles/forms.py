008f0bd439b321820a63fe14d4cee375715a2cad
BondarenkoAlexandr <sanyasashkabond32@gmail.com>
2021-10-12 16:57:40 +0300

    Forms for image upload with tags

--- src/articles/forms.py
+++ src/articles/forms.py
@@ -4,7 +4,7 @@ from django.utils.safestring import mark_safe
 
 class ArticleForm(forms.ModelForm):
     image = forms.FileField(
-            widget=forms.ClearableFileInput(attrs={'multiple': False}),
+            widget=forms.ClearableFileInput(attrs={'multiple': False, 'alt': 'Text'}),
             required=False,
             help_text='Необходимое количество фото - 7',
             label=mark_safe("<strong>Фото</strong>")
