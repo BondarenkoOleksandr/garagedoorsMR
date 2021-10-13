cca06a24601e4e5b0b7eb57f11a49dcd704a6df7
BondarenkoAlexandr <sanyasashkabond32@gmail.com>
2021-10-13 15:16:55 +0300

    Create gallery

--- src/gallery/admin.py
+++ src/gallery/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from gallery.models import Photo
 
-admin.site.register(Photo)
+
+class PhotoAdmin(admin.ModelAdmin):
+    fields = ('image_tag', 'alt', 'title', )
+    readonly_fields = ('image_tag',)
+
+
+admin.site.register(Photo, PhotoAdmin)
--- src/gallery/models.py
+++ src/gallery/models.py
@@ -7,3 +7,10 @@ class Photo(models.Model):
     image = models.ImageField(upload_to='gallery')
     alt = models.CharField(max_length=250)
     title = models.CharField(max_length=250)
+
+    def image_tag(self):
+        from django.utils.html import escape
+        return u'<img src="%s" />' % escape(self.image.url)
+
+    image_tag.short_description = 'Image'
+    image_tag.allow_tags = True
