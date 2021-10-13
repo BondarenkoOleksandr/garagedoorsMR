a241a275cfad89094562d97a5f9e41739e5e2a5d
BondarenkoAlexandr <sanyasashkabond32@gmail.com>
2021-10-13 15:24:55 +0300

    Create gallery

--- src/gallery/admin.py
+++ src/gallery/admin.py
@@ -6,7 +6,6 @@ from gallery.models import Photo
 
 class PhotoAdmin(admin.ModelAdmin):
     fields = ('image_tag', 'image', 'alt', 'title', )
-    readonly_fields = ('image_tag',)
 
 
 admin.site.register(Photo, PhotoAdmin)
