2f847d49e8cc9a043d1550a81920916f39e72ed0
BondarenkoAlexandr <sanyasashkabond32@gmail.com>
2021-10-13 16:36:12 +0300

    Create gallery

--- src/states/models.py
+++ src/states/models.py
@@ -4,6 +4,7 @@ from django.core.exceptions import ValidationError
 from django.db import models
 
 # Create your models here.
+from gallery.models import Photo
 from seo.models import SEOBase
 
 
@@ -28,6 +29,7 @@ class FirstScreen(models.Model):
     state = models.OneToOneField(to=State, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=250)
     description = models.TextField(max_length=500)
+    image = models.ForeignKey(Photo, on_delete=models.SET_NULL, null=True)
 
     class Meta:
         verbose_name_plural = "First Screen"
@@ -52,6 +54,7 @@ class ThirdScreen(models.Model):
     sec_description = models.TextField()
     thrd_title = models.CharField(max_length=250)
     thrd_description = models.TextField()
+    image = models.ForeignKey(Photo, on_delete=models.SET_NULL, null=True)
 
     class Meta:
         verbose_name_plural = "Third Screen"
