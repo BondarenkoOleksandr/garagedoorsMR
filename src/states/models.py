b14c90b3b9e5f80386d840c9765e21e184adb5cc
BondarenkoAlexandr <sanyasashkabond32@gmail.com>
2021-10-18 10:29:13 +0300

    Changed image field for states

--- src/states/models.py
+++ src/states/models.py
@@ -4,6 +4,7 @@ from django.core.exceptions import ValidationError
 from django.db import models
 
 # Create your models here.
+from gallery.models import Photo
 from seo.models import SEOBase
 
 
@@ -28,7 +29,6 @@ class FirstScreen(models.Model):
     state = models.OneToOneField(to=State, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=250)
     description = models.TextField(max_length=500)
-    image = models.ImageField(upload_to='states/first_screen/', blank=True, null=True)
 
     class Meta:
         verbose_name_plural = "First Screen"
@@ -53,7 +53,6 @@ class ThirdScreen(models.Model):
     sec_description = models.TextField()
     thrd_title = models.CharField(max_length=250)
     thrd_description = models.TextField()
-    image = models.ImageField(upload_to='states/third_screen/', blank=True, null=True)
 
     class Meta:
         verbose_name_plural = "Third Screen"
