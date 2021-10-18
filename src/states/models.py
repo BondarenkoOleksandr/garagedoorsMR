90cdfb7a12a582bab5e782b4181ff8440efd14d6
BondarenkoAlexandr <sanyasashkabond32@gmail.com>
2021-10-18 10:30:08 +0300

    Changed image field for cities

--- src/states/models.py
+++ src/states/models.py
@@ -29,6 +29,7 @@ class FirstScreen(models.Model):
     state = models.OneToOneField(to=State, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=250)
     description = models.TextField(max_length=500)
+    image = models.ForeignKey(Photo, on_delete=models.SET_NULL, null=True)
 
     class Meta:
         verbose_name_plural = "First Screen"
@@ -53,6 +54,7 @@ class ThirdScreen(models.Model):
     sec_description = models.TextField()
     thrd_title = models.CharField(max_length=250)
     thrd_description = models.TextField()
+    image = models.ForeignKey(Photo, on_delete=models.SET_NULL, null=True)
 
     class Meta:
         verbose_name_plural = "Third Screen"
